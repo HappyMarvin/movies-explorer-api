723d8585ba5f4facda1bde29fc8ab736b421884a
Happy Marvin <HappyMarvin@yandex.ru>
2021-06-08 10:20:56 +0300

    правки

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(requestLogger);
 app.set('trust proxy', 'loopback');
 app.use(limiter);
 app.use(cors({
-  origin: 'movies.happymarvin.ru',
+  origin: ['http://movies.happymarvin.ru', 'https://movies.happymarvin.ru', 'http://fr.localist.com:3000'],
   credentials: true,
 }));
 app.use(helmet());
